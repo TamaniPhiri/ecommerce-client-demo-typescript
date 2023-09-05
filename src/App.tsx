8091097d035e47f7fd71cf9f330e7d5f01c707b0
Tamani Phiri <tamanigabriel0@gmail.com>
2023-09-05 16:44:42 +0200

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import './App.css'
+import {Route,Routes} from 'react-router-dom'
 
 function App() {
 
   return (
     <>
-      app
+    <Routes>
+      <Route path='/'/>
+      </Routes>
     </>
   )
 }
