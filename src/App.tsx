1c728eea2106350ae7519de9f86bb28762874969
Tamani Phiri <tamanigabriel0@gmail.com>
2023-09-05 16:45:38 +0200

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import Nav from "./components/global/Nav";
+import Footer from "./components/global/Footer";
 
 function App() {
   return (
     <>
+      <Nav />
       <Routes>
         <Route path="/" Component={Home} />
       </Routes>
+      <Footer />
     </>
   );
 }
