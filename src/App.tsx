7ca907410d20c2eb36db9bbdc6904334b51b0200
Tamani Phiri <tamanigabriel0@gmail.com>
2023-09-05 16:45:18 +0200

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import './App.css'
 import {Route,Routes} from 'react-router-dom'
+import Home from './pages/Home'
 
 function App() {
 
   return (
     <>
     <Routes>
-      <Route path='/'/>
+      <Route path='/' Component={Home}/>
       </Routes>
     </>
   )
